1ce91ef119250ba793271fedcbe7360aa3559cd4
jerrychan807 <529883409@qq.com>
2018-08-22 23:43:30 +0800

    add code
    add code

--- pdd_spider_v2/pdd_spider_v2/spiders/category_infos.py
+++ pdd_spider_v2/pdd_spider_v2/spiders/category_infos.py
@@ -5,13 +5,14 @@ from os import path
 import json,Queue
 import sys,random
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
-from pdd_spider_v2.items import PddSpiderCategoryItem
+from pdd_spider_v2.items import PddSpiderSecCategoryItem
 from pdd_spider_v2 import mongo_db
 
 
 class CategoryInfosSpider(scrapy.Spider):
     name = 'category_infos'
     allowed_domains = ['yangkeduo.com']
+
 
     def set_crawl_url(self, opt_id, opt_type):
         # api_num:2,3,4
@@ -21,32 +22,36 @@ class CategoryInfosSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        #first_opt_id_list = [14,1281,4,15,1,1282,743,18,13,1917,818,1543,16,1451,2048,2478]
-        first_opt_id_list = [14]
+        first_opt_id_list = [14,1281,4,15,1,1282,743,18,13,1917,818,1543,16,1451,2048,2478]
+        # first_opt_id_list = [1281,4]
         for first_opt_id in first_opt_id_list:
-            self.opt_id = first_opt_id
-            self.first_info_dict = {"opt_id":self.opt_id,"cats":[]}
-            self.second_info_dict = {"opt_id":"","cats": []}
-            self.set_crawl_url(first_opt_id,opt_type=1)
+            self.set_crawl_url(first_opt_id, opt_type = 1)
             print "[*] url is {0}".format(self.url)
-            yield scrapy.Request(url=self.url, callback=self.parse, dont_filter=True)
+            yield scrapy.Request(url=self.url, callback=self.parse, dont_filter=True, meta={"first_opt_id": first_opt_id})
 
 
     def parse(self, response): # 处理一次一级opt_id的请求,获取二级分类信息
         html_json_data = json.loads(response.body.decode('utf-8'))
-        # first_cat_item = PddSpiderCategoryItem()
-        # self.first_info_dict['cats'] = [{"opt_id":opt_info['id'],"opt_name":opt_info['name']} for opt_info in html_json_data['opt_infos']] # 二级分类信息列表
+        first_opt_id = response.meta["first_opt_id"]
         for opt_info in html_json_data['opt_infos']:
-            self.second_info_dict = {"opt_id":opt_info['id'],"opt_name":opt_info['name']}
-            self.first_info_dict['cats'].append(self.second_info_dict)
-            self.set_crawl_url(opt_info['id'],opt_type=2)
+            second_info_dict = {"father_opt_id" : first_opt_id, "opt_id" : opt_info['id'],"opt_name" : opt_info['opt_name'], "cats": []}
+
+            self.set_crawl_url(opt_info['id'], opt_type = 2)
             print "[*] url is {0}".format(self.url)
-            yield scrapy.Request(url=self.url,callback=self.get_third_cats_info)
+            yield scrapy.Request(url=self.url, callback=self.get_third_cats_info, meta={"second_info_dict": second_info_dict}, dont_filter=True)
 
 
     def get_third_cats_info(self, response):  # 处理一次二级opt_id的请求,获取三级分类信息
+        second_cat_item = PddSpiderSecCategoryItem()
+        second_info_dict = response.meta["second_info_dict"]
         html_json_data = json.loads(response.body.decode('utf-8'))
         for opt_info in html_json_data['opt_infos']:
-            self.second_info_dict['cats'].append({"opt_id":opt_info['id'],"opt_name":opt_info['name']})
-        print self.first_info_dict
+            second_info_dict['cats'].append({"opt_id" : opt_info['id'], "opt_name" : opt_info['opt_name']})
+
+        second_cat_item['_id'] = second_info_dict['opt_id']
+        second_cat_item['opt_name'] = second_info_dict['opt_name']
+        second_cat_item['father_opt_id'] = second_info_dict['father_opt_id']
+        second_cat_item['cats'] = second_info_dict['cats']
+        yield second_cat_item
+
 
